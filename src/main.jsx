ad173b7336fdd0932f16e3275a0451ddbf510b82
YMIRUUUUU <106319341+YMIRUUUUU@users.noreply.github.com>
2025-08-16 05:22:47 +0200

    Initialize Vite React app with Tailwind and GitHub Pages deploy

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PaletteMuse from './PaletteMuse.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaletteMuse />
+  </React.StrictMode>,
+)
